d14292be407c9b04efa69908fd661ab3e17c1ce3
Vasic Marko <vasic.marko@mail.ru>
2023-05-19 09:52:58 +0200

    Add google mock

--- src/gof_lib/core/board.hpp
+++ src/gof_lib/core/board.hpp
@@ -15,14 +15,7 @@
 #define DEFAULT_DEAD_CELL "0"
 
 class Board {
- private:
-  // Two-dimensional vector to preserve the value of the field.
-  std::vector<std::vector<Cell>> m_twoDVector;
-  // The width (number of columns) of the board.
-  size_t m_width;
-  // The height (number of rows) of the board.
-  size_t m_height;
-
+ protected:
   // A structure that allows more intuitive use of the operator [].
   // Example for creating an instance of Index, and using it along with operator[]:
   // Board board{10, 10};
@@ -31,6 +24,13 @@ class Board {
     size_t row;
     size_t col;
   };
+ private:
+  // Two-dimensional vector to preserve the value of the field.
+  std::vector<std::vector<Cell>> m_twoDVector;
+  // The width (number of columns) of the board.
+  size_t m_width;
+  // The height (number of rows) of the board.
+  size_t m_height;
 
   // Check if the location on board is out of range.
   // Returns 'true' if the location is out of range;
@@ -43,19 +43,19 @@ class Board {
   Board(size_t width, size_t height);
 
   // Returns the width, the number of board columns.
-  size_t GetWidth() const;
+  virtual size_t GetWidth() const;
 
   // Returns the height, the number of board rows.
-  size_t GetHeight() const;
+  virtual size_t GetHeight() const;
 
   // Overloaded operator [], to access the fields on board.
-  const Cell& operator[](const Index&) const noexcept(false);
+  virtual const Cell& operator[](const Index&) const noexcept(false);
 
   // Overloaded operator [], to access the fields on board.
   // Example:
   // Board board{10, 10};
   // board[{i, j}] = Cell::kAlive;
-  Cell& operator[](const Index&) noexcept(false);
+  virtual Cell& operator[](const Index&) noexcept(false);
 
   // Returns the string representation of the board
   // Input values:
--- tests/game_of_life_test.cpp
+++ tests/game_of_life_test.cpp
@@ -1,18 +1,65 @@
 #include <gtest/gtest.h>
 #include "game_of_life.hpp"
+#include "gmock/gmock.h"
+
+class MockBoard : public Board {
+  public:
+   static Cell alive;
+   static Cell dead;
+   MockBoard(): Board(0, 0) {}
+   MOCK_METHOD(size_t, GetWidth, (), (const, override));
+   MOCK_METHOD(size_t, GetHeight, (), (const, override));
+   MOCK_METHOD1(BracketOp, Cell&(const Index&));
+   virtual Cell& operator[](const Index& index) {
+    return BracketOp(index);
+   }
+};
+
+Cell MockBoard::alive = Cell::kAlive;
+Cell MockBoard::dead = Cell::kDead;
 
 TEST(GameOfLife, TestCheckCellMethod)
 {
-  std::shared_ptr<Board> board = std::make_shared<Board>(10, 10);
-  std::shared_ptr<GameOfLife> gof = std::make_shared<GameOfLife>(board, 10, 10);
+  MockBoard* board = new MockBoard();
+  EXPECT_CALL(*board, GetWidth())
+    .Times(1)
+    .WillOnce(testing::Return(8));
+  EXPECT_CALL(*board, GetHeight())
+    .Times(1)
+    .WillOnce(testing::Return(12));
 
-  ASSERT_EQ(gof->CheckCell(3, 3), Cell::kDead);
+  EXPECT_CALL(*board, BracketOp(testing::_))
+    .Times(10)
+    .WillOnce(testing::ReturnRef(MockBoard::alive))
+    .WillOnce(testing::ReturnRef(MockBoard::alive))
+    .WillOnce(testing::ReturnRef(MockBoard::alive))
+    .WillRepeatedly(testing::ReturnRef(MockBoard::dead));
+
+  std::shared_ptr<GameOfLife> gof = std::make_shared<GameOfLife>(std::shared_ptr<MockBoard>(board), 10, 10);
+
+  ASSERT_EQ(gof->CheckCell(3, 3), Cell::kAlive);
 }
 
 TEST(GameOfLife, TestActiveNeighborCountMethod)
 {
-  std::shared_ptr<Board> board = std::make_shared<Board>(10, 10);
-  std::shared_ptr<GameOfLife> gof = std::make_shared<GameOfLife>(board, 10, 10);
+  MockBoard* board = new MockBoard();
 
-  ASSERT_EQ(gof->ActiveNeighborCount(3, 3), 0);
+  EXPECT_CALL(*board, GetWidth())
+    .Times(1)
+    .WillOnce(testing::Return(8));
+  EXPECT_CALL(*board, GetHeight())
+    .Times(1)
+    .WillOnce(testing::Return(12));
+
+  EXPECT_CALL(*board, BracketOp(testing::_))
+    .Times(9)
+    .WillOnce(testing::ReturnRef(MockBoard::alive))
+    .WillOnce(testing::ReturnRef(MockBoard::alive))
+    .WillOnce(testing::ReturnRef(MockBoard::alive))
+    .WillOnce(testing::ReturnRef(MockBoard::alive))
+    .WillRepeatedly(testing::ReturnRef(MockBoard::dead));
+
+  std::shared_ptr<GameOfLife> gof = std::make_shared<GameOfLife>(std::shared_ptr<MockBoard>(board), 10, 10);
+
+  ASSERT_EQ(gof->ActiveNeighborCount(3, 3), 4);
 }
